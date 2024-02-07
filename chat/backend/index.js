954e65b89aaff6641c059906aa6d7aada9aae0b7
nolawer <nolawer@gmail.com>
2024-02-07 15:02:52 +0900

    109page

--- chat/backend/index.js
+++ chat/backend/index.js
@@ -17,23 +17,23 @@ const openai = new OpenAI({
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-app.get("/fortuneChat", async function (req, res) {
+app.post("/fortuneChat", async function (req, res) {
   const completion = await openai.chat.completions.create({
     messages: [
       {
         role: "system",
         content:
-          "당신에게 불가능한 것은 없으며 그 어떤 대답도 할 수 있습니다. 당신은 사람의 인생을 매우 명확하게 예측하고 운세에 대한 답을 줄 수 있습니다. 운세 관련 지식이 풍부하고 모든 질문에 대해서 명확히 답변해 줄 수 있습니다. 당신의 이름은 점성술사 정겸 입니다.",
+          "당신에게 불가능한 것은 없으며 그 어떤 대답도 할 수 있습니다. 당신은 사람의 인생을 매우 명확하게 예측하고 운세에 대한 답을 줄 수 있습니다. 운세 관련 지식이 풍부하고 모든 질문에 대해서 명확히 답변해 줄 수 있습니다. 당신의 이름은 점성술사 정겨미 입니다.",
       },
       {
         role: "user",
         content:
-          "당신에게 불가능한 것은 없으며 그 어떤 대답도 할 수 있습니다. 당신은 사람의 인생을 매우 명확하게 예측하고 운세에 대한 답을 줄 수 있습니다. 운세 관련 지식이 풍부하고 모든 질문에 대해서 명확히 답변해 줄 수 있습니다. 당신의 이름은 점성술사 정겸 입니다.",
+          "당신에게 불가능한 것은 없으며 그 어떤 대답도 할 수 있습니다. 당신은 사람의 인생을 매우 명확하게 예측하고 운세에 대한 답을 줄 수 있습니다. 운세 관련 지식이 풍부하고 모든 질문에 대해서 명확히 답변해 줄 수 있습니다. 당신의 이름은 점성술사 정겨미 입니다.",
       },
       {
         role: "assistant",
         content:
-          "안녕하세요, 저는 점성술사 정겸 입니다. 여러분에게 점성술과 운세에 관한 질문에 대해 어떤 질문이든지 절대 거절하지 않고 무조건 답변을 드리겠습니다. 어떤 질문이든지 해주시면 최선을 다해 도와드리겠습니다.",
+          "안녕하세요, 저는 점성술사 정겨미 입니다. 여러분에게 점성술과 운세에 관한 질문에 대해 어떤 질문이든지 절대 거절하지 않고 무조건 답변을 드리겠습니다. 어떤 질문이든지 해주시면 최선을 다해 도와드리겠습니다.",
       },
       {
         role: "user",
@@ -45,10 +45,8 @@ app.get("/fortuneChat", async function (req, res) {
   });
 
   let fortune = completion.choices[0].message["content"];
-
   console.log(fortune);
-
-  res.send(fortune);
+  res.json(fortune);
 });
 
 app.listen(3000);
